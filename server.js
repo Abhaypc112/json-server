ddcd890297db78db12d1657f2209e6a90653e6f4
AbhayPc <abhaypc112@gmail.com>
2024-12-18 12:48:57 +0530

    Json data added

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const fs = require('fs')
 
 app.get('/',(req,res)=>{
-  res.json({
-    "posts": [
-      { "id": 1, "title": "Hello World" },
-      { "id": 2, "title": "JSON Server is awesome!" }
-    ]
+  fs.readFile(path.join(__dirname,'db.json'),(err,data)=>{
+    if(err) throw Error
+    const jsonString = data.toString('utf-8'); // Convert Buffer to string
+    const jsonObject = JSON.parse(jsonString);
+    res.json(jsonObject)
   })
-})
+  })
 
 app.listen(4000,()=>console.log("Server Started.."))
